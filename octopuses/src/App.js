1ab41c99330e1acff247fb5ddb5651a5ddc21a90
Isac Ciobota <ciobotaisac@gmail.com>
2022-02-14 19:09:28 +0200

    Init project + navbar

---
+++ octopuses/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavigationBar from './components/NavigationBar.js'
+
+function App() {
+  return (
+    <div className="App">
+        <NavigationBar/>
+    </div>
+  );
+}
+
+export default App;
