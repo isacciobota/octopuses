95ddc6a7cd44271cff578a9b82e3aeec26ef6934
Isac Ciobota <ciobotaisac@gmail.com>
2022-02-15 13:18:20 +0200

    Navbar Completed Staged Changes

--- octopuses/src/App.js
+++ octopuses/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar.js'
+import background from './resources/Background.jpeg'
 
 function App() {
   return (
     <div className="App">
         <NavigationBar/>
+        <img src={background} width="100%"></img>
     </div>
   );
 }
--- octopuses/src/components/NavigationBar.js
+++ octopuses/src/components/NavigationBar.js
@@ -1,39 +1,70 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import { NavItem, NavDropdown, MenuItem, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import {Nav} from 'react-bootstrap'
 import {Container} from "reactstrap"
+import {makeStyles} from "@material-ui/core/styles"
 
 //Socials
 import  instagram  from '../resources/instagramLogo.svg'
 import  discord  from '../resources/DiscordLogo.svg'
 import  twitter  from '../resources/TwitterLogo.svg'
 import  reddit  from '../resources/RedditLogo.svg'
+import  logo  from '../resources/Octopus.svg'
+
+const styles = makeStyles({
+  navItem:{
+    marginLeft: "20px",
+    color: "grey",
+    "&:hover":{
+      color:"black",
+     },
+  },
+  navSocials:{
+    marginRight: "20px",
+    width: "17px",
+    height: "17px",
+    
+    "&:hover":{
+     color: "red",
+    },
+  },
+})
 
 function NavigationBar() {
+    const classes = styles()
     return (
-      <Navbar bg="light" variant="light">
-        <Container>
-        <Nav className="me-auto" >
-          <Nav.Link href="#pricing">
-            <img src={instagram} width="20" height="20" className="d-inline-block align-top" /> 
-          </Nav.Link>
-          <Nav.Link href="#pricing">
-            <img src={discord} width="20" height="20" className="d-inline-block align-top" /> 
-          </Nav.Link>
-          <Nav.Link href="#pricing">
-           <img src={twitter} width="20" height="20" className="d-inline-block align-top" /> 
-          </Nav.Link>
-          <Nav.Link href="#pricing">
-            <img src={reddit} width="20" height="20" className="d-inline-block align-top" /> 
-          </Nav.Link>
+      <Navbar collapseOnSelect expand="lg" bg="white" variant="light" sticky="top">
+        <Container fluid> 
+          <Navbar.Brand href="#" style={{marginLeft: "10px"}}>
+           <img src={logo} alt="logo" width="60" height="60"/>
+          </Navbar.Brand>
+          
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{marginRight: "10px"}}/>
+          <Navbar.Collapse id="responsive-navbar-nav" placement="end">
+            <Nav className="m-auto" >
+              <Nav.Link href="#about" className={classes.navItem}>About</Nav.Link>
+              <Nav.Link href="#opensea" className={classes.navItem}>OpenSea</Nav.Link>
+              <Nav.Link href="#collection" className={classes.navItem}>Collection</Nav.Link>
+              <Nav.Link href="#team" className={classes.navItem}>Team</Nav.Link>
+              <Nav.Link href="#contact" className={classes.navItem}>Contact</Nav.Link>
+            </Nav>
 
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#features">OpenSea</Nav.Link>
-          <Nav.Link href="#pricing">Collection</Nav.Link>
-          <Nav.Link href="#pricing">Team</Nav.Link>
-          <Nav.Link href="#pricing">Contact</Nav.Link>
+            <Nav className="d-none d-lg-flex">
+              <Nav.Link href="#pricing">
+                <img src={instagram} alt="instagram" className={classes.navSocials}/> 
+              </Nav.Link>
+              <Nav.Link href="#pricing">
+                <img src={discord} alt="discord" className={classes.navSocials}/> 
+              </Nav.Link>
+              <Nav.Link href="#pricing">
+              <img src={twitter} alt="twitter" className={classes.navSocials}/> 
+              </Nav.Link>
+              <Nav.Link href="#pricing">
+                <img src={reddit} alt="reddit" className={classes.navSocials} style={{marginRight: "10px"}}/> 
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
 
-        </Nav>
         </Container>
       </Navbar>
     )
