bbe0ff07ce9e02d5f891a05b1fdebb3784fc5afa
Isac Ciobota <ciobotaisac@gmail.com>
2022-06-11 11:42:17 +0300

    last build skin working

--- octopuses/src/App.js
+++ octopuses/src/App.js
@@ -15,7 +15,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 export default function App() {
--- octopuses/src/index.js
+++ octopuses/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Home from './components/Home';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//pages
+import App from './App';
+import Home from './components/Home.js'
+import Skin from './components/Skin.js'
+import Accessories from './components/Accessories.js'
+import Clothes from './components/Clothes.js'
+import Eyes from './components/Eyes.js'
+import Mouth from './components/Mouth.js'
+import Tentacles from './components/Tentacles.js'
+import Brows from './components/Brows.js'
+import Background from './components/Background.js'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <Home /> }> </Route>
+        <Route path="/" element={<Home />}> </Route>
+        <Route path="/skin" element={<Skin />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/clothes" element={<Clothes />} />
+        <Route path="/eyes" element={<Eyes />} />
+        <Route path="/mouth" element={<Mouth />} />
+        <Route path="/tentacles" element={<Tentacles />} />
+        <Route path="/brows" element={<Brows />} />
+        <Route path="/background" element={<Background />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
